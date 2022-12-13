43aba6d14a4a536805880ef64f5ec65f14983ee2
Purujit Bansal <purujit.bansal@external.swiggy.in>
2022-12-13 13:44:36 +0530

    removed comments

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
-import { View, Text, Button, StyleSheet, FlatList, useWindowDimensions, StatusBar, Animated } from 'react-native'
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
-import HomeCarousel from './components/HomeCarousel'
+import React from 'react'
+import { View, Button } from 'react-native'
 import InfiniteScrollDisplay from './screens/InfiniteScrollDisplay'
 import SwipeModalDisplay from './screens/SwipeModalDisplay'
-
-function getColor() {
-  // generates random pastel color
-  return 'hsl(' + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)'
-}
 
 function HomeScreen({ navigation }: { navigation: any }) {
   return (
@@ -32,28 +25,8 @@ function HomeScreen({ navigation }: { navigation: any }) {
 }
 
 const Stack = createNativeStackNavigator()
-
 const App = () => {
-  // const { height, width } = useWindowDimensions()
-  // const flatlistElement = useRef()
-  // const [tabs, setTabs] = useState([getColor(), getColor()])
-
-  // const addTab = useCallback(() => {
-  //   setTabs([...tabs, getColor()])
-  // }, [tabs])
-  // const getWrappableData = () => {
-  //   console.log([tabs[tabs.length - 1], ...tabs, tabs[0]])
-  //   return [tabs[tabs.length - 1], ...tabs, tabs[0]]
-  // }
-
   return (
-    // <View style={{ flex: 1, justifyContent: 'space-around', marginTop: StatusBar.currentHeight }}>
-    //   <Button onPress={addTab} title="Add Tab" />
-    //   <Text>Total {tabs.length} Tabs</Text>
-    //   <View style={{ flex: 1 }}>
-    //     <HomeCarousel data={tabs} />
-    //   </View>
-    // </View>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home Ground">
         <Stack.Screen name="Home Ground" component={HomeScreen} />
